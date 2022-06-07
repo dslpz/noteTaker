9610c953af6b9dfa05c10f0cd8be3e424a29969f
Damaris Palacios <dslpz728@gmail.com>
2022-06-07 18:04:57 -0500

    start to note.js

---
+++ Develop/notes.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const express = require('express')
+const router = express.Router()
+const db = require('../db/notes.json');
+const path = require('path');
+
+
+
+
+
+module.exports = router
