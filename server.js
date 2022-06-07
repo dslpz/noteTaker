7c0a156c3a469004502e4448a9aec6ab04f2df16
Damaris Palacios <dslpz728@gmail.com>
2022-06-07 17:51:20 -0500

    set up route

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const app = express();
 
+const { notes } = require('./Develop/notes');
 
-
-
-
-
-
-
-
-
-
-
-
+app.get('/index.html', (req, res) => {
+    res.json(notes);
+  });
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
